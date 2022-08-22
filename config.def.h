46a5513a083dfeb04e8ab549c4684c08df6afdde
Alexandr "MrSteyk" German <mrsteyk1@vk.com>
2022-08-22 11:56:11 +0500

    Theme changes

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#bac2de", "#1e1e2e" },
+	[SchemeSel] = { "#45475a", "#94e2d5" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
